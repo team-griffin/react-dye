2c738aad6852afcb39051f12926208ac9a81439a
thd <tharisondenby@godaddy.com>
2019-11-05 15:58:45 +0000

    fix: missing exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ThemeProvider from './ThemeProvider';
 import withTheme from './withTheme';
+import useTheme from './useTheme';
+import withThemeHook from './withThemeHook';
 
 export {
   ThemeProvider,
   withTheme,
+  withThemeHook,
+  useTheme,
 };
